6631971b622b537e3c51830253e0f87ff6a2ecf1
XHT1227 <115413802+XHT1227@users.noreply.github.com>
2022-10-10 12:50:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,92 @@
-from datetime import date, datetime
-import math
-from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
-import requests
+from datetime import date, datetime, timedelta
+from wechatpy import WeChatClient, WeChatClientException
+from wechatpy.client.api import WeChatMessage
 import os
-import random
+from util import get_accmulation_str, get_countdown_str, get_random_color, get_my_word, get_weather_str, get_week_day
 
-today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+#
+# 变量
+nowtime = datetime.utcnow() + timedelta(hours=8)  # 东八区时间
+today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d") #今天的日期
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+#
+# 系统变量
+app_id = os.getenv('APP_ID')
+app_secret = os.getenv('APP_SECRET')
+user_ids = os.getenv('USER_ID', '').split("\n")
+template_id = os.getenv('TEMPLATE_ID')
 
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
+citys = os.getenv('CITY', '').split("\n")
+print("CITY: ", citys)
+accumulation = os.getenv('SUMCNT', '').split("\n")
+print("SUMCNT: ", accumulation)
+countdown = os.getenv('COUNTDOWN', '').split("\n")
+print("COUNTDOWN: ", countdown)
 
+# check env vars
+if app_id is None or app_secret is None:
+  print('请设置 APP_ID 和 APP_SECRET')
+  exit(422)
+if not user_ids:
+  print('请设置 USER_ID，若存在多个 ID 用回车分开')
+  exit(422)
+if template_id is None:
+  print('请设置 TEMPLATE_ID')
+  exit(422)
 
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+#
+# text data
+#
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+data = {
+  "date": {
+    "value": today.strftime('%Y年%m月%d日'),
+    "color": get_random_color()
+  },
+  "week_day": {
+    "value": get_week_day(today),
+    "color": get_random_color()
+  },
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+  "combine_weather": {
+    "value": get_weather_str(citys),
+    "color": get_random_color()
+  },
+  "combine_accumulation_count": {
+    "value": get_accmulation_str(accumulation, today),
+    "color": get_random_color()
+  },
+  "combine_countdown": {
+    "value": get_countdown_str(countdown, today, nowtime),
+    "color": get_random_color()
+  },
 
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  "words": {
+    "value": get_my_word(),
+    "color": get_random_color()
+  },
+}
 
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+#
+# main
+#
 
+if __name__ == '__main__':
+  try:
+    client = WeChatClient(app_id, app_secret)
+  except WeChatClientException as e:
+    print('微信获取 token 失败，请检查 APP_ID 和 APP_SECRET，或当日调用量是否已达到微信限制。')
+    exit(502)
 
-client = WeChatClient(app_id, app_secret)
+  wm = WeChatMessage(client)
+  count = 0
+  try:
+    for user_id in user_ids:
+      print('正在发送给 %s, 数据如下：%s' % (user_id, data))
+      res = wm.send_template(user_id, template_id, data)
+      count+=1
+  except WeChatClientException as e:
+    print('微信端返回错误：%s。错误代码：%d' % (e.errmsg, e.errcode))
+    exit(502)
 
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+  print("发送了" + str(count) + "条消息")
